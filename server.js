ebb3fb469839146af1c858db374218571fc8f332
MahmoudMahfouz-1 <mahmoud1mahfouz@gmail.com>
2024-11-28 19:15:06 +0200

    added some security layers to the project - hpp - mongosanitize - xss

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const morgan = require('morgan');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const compression = require('compression');
+const { rateLimit } = require('express-rate-limit');
+const hpp = require('hpp');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 dotenv.config();
 
@@ -35,9 +39,34 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
   console.log(`Current Mode is ${process.env.NODE_ENV}`);
 }
-app.use(express.json());
+
+// limiting the maximum size of the body of the request
+app.use(express.json({ limit: '20kb' }));
 app.use(express.static(__dirname, { path: 'uploads' }));
 
+// prevent MongoDB Operator Injection.
+app.use(mongoSanitize());
+app.use(xss());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100,
+  message: 'You can only make 15 requests every hour.',
+});
+app.use('/api', limiter);
+
+// Express middleware to protect against HTTP Parameter Pollution attacks
+app.use(
+  hpp({
+    whitelist: [
+      'sold',
+      'price',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'quantity',
+    ],
+  })
+);
 // Mount Routes
 mountRoutes(app);
 
